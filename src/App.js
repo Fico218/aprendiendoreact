265902a8b03ed0bef356ea129afbef22e15432fa
Jorge Sandoval <jrss317031@gmail.com>
2023-09-02 04:40:29 -0500

    public/index.html src/App.js src/App.css -> src/assets/css/App.css src/index.css -> src/assets/css/index.css src/logo.svg -> src/assets/images/logo.svg src/components/MiComponente.js src/index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+// Importar componentes
+import MiComponente from './components/MiComponente';
+
+function HolaMundo(nombre, edad){
+  var presentacion = (
+    <div>
+      <h2>Hola, soy {nombre}</h2>
+      <h3>Tengo {edad} años</h3>
+    </div>
+      );
+  return presentacion; 
+}
 
 function App() {
+  var nombre = "Fico Sandoval"; 
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola bienvenidos al curso de react de Victor Robles WEB. <code>Curso tomado por Jorge Sandoval</code>🚀
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {HolaMundo(nombre, 54)}
+     
+      <section className='componentes'>
+        <MiComponente /> 
+    
+      </section>
+
       </header>
+     
+      <div></div>
     </div>
   );
 }
